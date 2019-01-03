461118b3be5db806dd366b089d19fa7934990dde
joe kabucho <joekabucho2@gmail.com>
2019-01-03 12:59:22 +0300

    first game commit

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+public bool hasDied;
+
+	
+	// Update is called once per frame
+	void Update () {
+	 if(gameObject.transform.position.y < -7){
+		Die();
+	 }
+	
+	}
+
+	void Die () {
+	SceneManager.LoadScene("prototype1");
+
+	}
+}
