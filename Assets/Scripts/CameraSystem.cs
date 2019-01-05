c7f6b0fac8deea2455aa7fe187d6491f639d0b25
joe kabucho <joekabucho2@gmail.com>
2019-01-05 13:33:15 +0300

    beta version

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -11,13 +11,19 @@ public class CameraSystem : MonoBehaviour {
  
 	// Use this for initialization
 	void Start () {
+		if(GameObject.FindGameObjectWithTag ("Player")!=null)
+		{
 		player = GameObject.FindGameObjectWithTag ("Player");
+		}
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
+		if(GameObject.FindGameObjectWithTag ("Player")!=null)
+		{
 		float x = Mathf.Clamp(player.transform.position.x,xMin,xMax);
 		float y = Mathf.Clamp(player.transform.position.y,yMin,yMax);
 		gameObject.transform.position=new Vector3 (x,y,gameObject.transform.position.z);
+		}
 	}
 }
---
+++ Assets/Scripts/CharacterSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] players;
+	void Start () {
+		if (PlayerPrefs.GetInt("SelectedCharacter")==0){
+			Instantiate (players[(0)],Vector2.zero,Quaternion.identity);
+		}
+			if (PlayerPrefs.GetInt("SelectedCharacter")==1){
+			Instantiate (players[(1)],Vector2.zero,Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+
+}
